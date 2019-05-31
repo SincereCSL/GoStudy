b1c48beb86849f514f853cf979047046d04f0741
SincereCSL <paranoid88@.126.com>
2019-05-31 16:41:44 +0800

    学习go语法

---
+++ GoDemo/src/demo/go_grammar.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//字符串连接
+func main()  {
+	fmt.Println("Google"+"go")
+	//Go语言变量
+	var a string = "learn go"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b,c)
+}
+
